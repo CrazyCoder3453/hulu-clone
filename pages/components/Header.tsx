917616aa694ef73db654185c2cf31f49f012d1c9
= <=>
2023-04-16 22:10:38 -0500

    header

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import Image from 'next/image'
+import HeaderItem from './HeaderItem'
 
 function Header() {
   return (
     <header>
-        Header
+        <div>
+            <HeaderItem title="HOME" ICON={}/>
+        </div>
+        <Image
+            className='object-contain' 
+            src='https://links.papareact.com/ua6'
+            width={200}
+            height={100}
+            alt='hulu-logo'
+        />
     </header>
   )
 }
